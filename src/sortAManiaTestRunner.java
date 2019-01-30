a0d23982892c3ee4a769a51c4b37f4b19b186f55
BT_1N3_13 <a>
2019-01-30 08:19:30 -0500

    Created runner class

---
+++ src/sortAManiaTestRunner.java
@@ -0,0 +1,20 @@
+public class sortAManiaTestRunner {
+    public static void main(String[] args){
+        SortCompetition team1 = new Team1SortCompetition(); // create an instance of your class
+        int[] randIntArr = randomIntsArr(10000); // initialize a random int array
+        String[] randStringArr = randomStringArr(10000,5); //init a random string array
+
+        System.out.println("Unsorted");
+        printArr(randIntArr); //print out the unsorted int array
+
+        long time = System.currentTimeMillis(); //start the timer
+        int median = team1.challengeOne(randIntArr); //run your challenge one code
+        time = System.currentTimeMillis() - time; // stop the timer
+        System.out.println("Challenge One Time Taken: " + time * 0.001 + " Seconds"); //print the time
+        System.out.println("Median equals: " + median); //print the median you found
+
+        System.out.println("Sorted");
+        printArr(randIntArr); //check if the array was sorted correctly
+
+    }
+}
