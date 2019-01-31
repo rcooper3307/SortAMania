4a0c0365d68e545a902676c04913e7bf70d3fc40
gkr <greynolds7248@bths.edu>
2019-01-31 08:39:33 -0500

    started challenge 4

--- src/ChallengeFour.java
+++ src/ChallengeFour.java
@@ -1,5 +1,18 @@
 public abstract class ChallengeFour extends Team3SortCompetition {
     public ChallengeFour(int[][] Arr) {
-        
+        challengeFour(Arr);
+    }
+    @Override
+    public int challengeFour(int[][] arr) {
+
+    }
+    public void ArraySort(int[][] arr1) {
+        int[] temp = new int[1000];
+        for(int i = 0; i < 1000; i++) {
+            for(int j = 0; j < 1000; j++) {
+                temp[j] = arr1[j][i];
+            }
+            Sorts.insertionSort(temp);
+        }
     }
 }
